2f5feaa9f1cb2b0c1f0662f79bb567379a273d42
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 22:01:39 +0800

    added as_view and specified the exact imports in urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import recipe_list, recipe
 
 urlpatterns = [
-    path('recipes/list', recipe_list, name='recipe_list'),
-    path('recipe/<int:num>', recipe, name='recipe'),
+    path('recipes/list', recipe_list.as_view(), name='recipe_list'),
+    path('recipe/<int:num>', recipe.as_view(), name='recipe'),
 ]
