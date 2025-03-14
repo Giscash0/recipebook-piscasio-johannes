3821160ae2149839a31fc20ad947b20ff3cb90fc
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 23:16:36 +0800

    updated recipe.html and changed num to pk in urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,5 +3,5 @@ from .views import recipe_list, recipe
 
 urlpatterns = [
     path('recipes/list', recipe_list.as_view(), name='recipe_list'),
-    path('recipe/<int:num>', recipe.as_view(), name='recipe'),
+    path('recipe/<int:pk>', recipe.as_view(), name='recipe'),
 ]
