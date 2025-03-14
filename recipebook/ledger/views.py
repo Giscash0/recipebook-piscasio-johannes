910bf9a6893361e8ad2feffd9964cf3c21b29ae9
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 21:58:49 +0800

    fixed views.py by changing def to class

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,11 +2,11 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Recipe
 
-def recipe_list(ListView):
+class recipe_list(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
     context_object_name = 'recipes'
 
-def recipe(request, num=1):
+class recipe(DetailView):
     model = Recipe
     template_name = 'recipe.html'
