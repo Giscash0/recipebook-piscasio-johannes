0d62afe5c4544b6eb3bfddbb8cdd8e0fe7d0c3f7
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 11:03:38 +0800

    implemented recipe list

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", basicTemplate, name="basicTemplate")
+    path('recipes/list', recipe_list, name='recipe_list'),
 ]
