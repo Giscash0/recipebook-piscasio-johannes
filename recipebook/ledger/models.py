e03739eb8835d34fa26b01ac282cbb07781c1373
Giscash0 <jmgpiscasio@gmail.com>
2025-03-14 22:34:47 +0800

    added 'on_delete=models.CASCADE' as argument for ForeignKey

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -21,8 +21,8 @@ class Recipe(models.Model):
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
-    ingredient = models.ForeignKey(Ingredient, related_name='recipe')
-    recipe = models.ForeignKey(Recipe, related_name='ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient} for {self.recipe}"
