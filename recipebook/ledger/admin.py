3251e48aeb2e27cd6622a2bb7269389d679215a0
Giscash0 <jmgpiscasio@gmail.com>
2025-05-23 23:55:50 +0800

    added RecipeAdmin in admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
@@ -17,10 +17,14 @@ class ProfileInline(admin.StackedInline):
 
 class UserAdmin(admin.ModelAdmin):
 	inlines = [ProfileInline]
+     
+class RecipeAdmin(admin.ModelAdmin):
+     model = RecipeImage
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(RecipeImage, RecipeAdmin)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
