c5a4a010f5d11260107df4b2bc87d352dbfa961f
Giscash0 <jmgpiscasio@gmail.com>
2025-05-24 00:30:21 +0800

    modified RecipeList view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Recipe
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm, IngredientFormSet
 
 class RecipeList(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipe_list.html'
     context_object_name = 'recipes'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['can_add_recipe'] = True
+        return context
 
 class RecipeDetail(LoginRequiredMixin, DetailView):
     model = Recipe
